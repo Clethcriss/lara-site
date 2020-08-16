1575913cae26e7b78d9c47986274fb014909fa53
Balazs Borsi <balazs.borsi@guava.blue>
2020-08-16 23:07:59 +0200

    on hover img size will be bigger

--- src/component/GalleryComponent.tsx
+++ src/component/GalleryComponent.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
+import '../style/gallery.css';
 
 export interface IGalleryComponentProps {
 
 }
 
 export interface IGalleryComponentState {
-
+    images: string[];
 }
 
 class GalleryComponent extends React.Component<IGalleryComponentProps, IGalleryComponentState> {
     constructor(props: IGalleryComponentProps) {
         super(props);
 
-        this.state = {}
+        this.state = {
+            images: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            images: ['images/chihiro.jpg', 'images/kovacs.jpg', 'images/kutyi.jpg']
+        });
     }
 
     render() {
+        const {images} = this.state;
         return (
-            <div>
+            <div className="gallery">
                 <h1>Here will be the Gallery!</h1>
+                {images.map((img, index) => {
+                    return <img height='240px' width='180px' src={img} alt={`kepcske-${index}`}/>
+                })}
             </div>
         );
     }
